cfd921220d81164e541ca134853c097d1867af62
Romesh Niriella <romesh.niriella@beteasy.com.au>
2019-10-17 20:56:36 +1100

    test  setup

--- tests/EthereumApi.Tests/UnitTest1.cs
+++ tests/EthereumApi.Tests/UnitTest1.cs
@@ -1,12 +1,29 @@
+using System.Threading.Tasks;
 using Xunit;
 
 namespace EthereumApi.Tests
 {
-    public class UnitTest1
+    public class InvalidInputTheories : TheoryData<string, InvalidInputTheoryData>
     {
-        [Fact]
-        public void Test1()
+    }
+
+    public class InvalidInputTheoryData
+    {
+    }
+
+    public class TestServiceProvider
+    {
+    }
+
+    public class When_Searching_For_A_Block
+    {
+        public class And_The_Data_Is_Invalid
         {
+            [Theory]
+            [ClassData(typeof(InvalidInputTheories))]
+            public async Task It_Throws_Exceptions(string description, InvalidInputTheoryData theoryData)
+            {
+            }
         }
     }
 }
