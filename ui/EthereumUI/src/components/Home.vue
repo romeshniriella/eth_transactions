d6844c4a80039b2f4fe9922dde0b314543df36d2
Romesh Niriella <romesh.niriella@beteasy.com.au>
2019-10-19 00:33:57 +1100

    UI - skeleton

--- ui/EthereumUI/src/components/Home.vue
+++ ui/EthereumUI/src/components/Home.vue
@@ -1,17 +1,39 @@
 <template>
-    <div class="home">
-        <h1>{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
-    </div>
+  <div class="home">
+    <h4>{{ msg }}</h4>
+    <form class="mui-form--inline">
+      <div class="mui-textfield mui-textfield--float-label">
+        <input type="text" /> // bind to these inputs - props
+        <label>Block Number</label>
+      </div>
+      <div class="mui-textfield mui-textfield--float-label"> 
+        <input type="text" value /> // bind to these inputs - props
+        <label>Address e.g: 0xffffffffffffffffffffffffffffff</label>
+      </div>
+      <button class="mui-btn">submit</button> // click this button - event handler
+    </form>
+    <br/>
+    <Transactions />
+  </div>
 </template>
 
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Transactions from "../components/Transactions.vue";
 
-    @Component
-    export default class Home extends Vue {
-        @Prop() private msg!: string;
-    }
+@Component({
+  components: {
+    Transactions
+  }
+})
+export default class Home extends Vue {
+  @Prop() private msg!: string;
+  // submit event handler
+  // props to bind to search inputs
+  // method to get data from server
+  // logic to put data in transactions component
+  //    Transactions component -> render data when I pass in data
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ ui/EthereumUI/src/components/Transactions.vue
@@ -0,0 +1,43 @@
+<template>
+    <table class="mui-table mui-table--bordered">
+<thead>
+    <tr>
+      <th>Block Hash</th>
+      <th>Block Number</th>
+      <th>Gas</th>
+      <th>Hash</th>
+      <th>From</th>
+      <th>To</th>
+      <th>Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Cell 1-1</td>
+      <td>Cell 1-2</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Cell 2-1</td>
+      <td>Cell 2-2</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+    </table>
+</template>
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Transactions extends Vue {
+      
+  }
+</script>
